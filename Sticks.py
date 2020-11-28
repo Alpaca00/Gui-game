79fdbd3cddc33be9ac5c147bb037f1eae636914e
Oleg <alpaca00tuha@gmail.com>
2020-11-28 21:04:21 +0200

    New change

--- Sticks.py
+++ Sticks.py
@@ -6,7 +6,7 @@ import random
 root = Tk()
 root.geometry('400x400+440+200')
 root.title('TakeSticks')
-root.resizable(False, False)
+#root.resizable(False, False)
 
 
 pb = t.Progressbar(root, length=1000)
@@ -23,8 +23,11 @@ e = Entry(root, bg='black', fg='Teal')
 e.pack(fill=X)
 
 
-rules = messagebox.showinfo('Rules', 'The game is who takes the last stick is a loser.\nAccording to the rules, the player must take 1 to 3 sticks.\nA game starts with a random number.')
+rules = messagebox.showinfo('Rules', """The game is who takes the last stick is a loser.
+\nAccording to the rules, the player must take 1 to 3 sticks.\n
+A game starts with a random number.""")
 
+num = [14, 15, 16, 18, 23, 24, 28]
 
 text1=Text(root, font='cursive, 10', wrap=WORD, bg='black', fg='white')
 text1.pack(fill=BOTH, expand=True)
@@ -47,9 +50,9 @@ def quit():
     if answer == True:
         root.destroy()
 
+
+amount = random.randint(13, 30)
         
-x = random.randint(13, 30)
-amount = round(random.randint(13, 30), 4*x+1)
 
 def get_num(number):
     global amount
@@ -70,14 +73,14 @@ def get_num(number):
         taken_max = 1
     amount -= taken_max     
     text1.insert(END, f'Artificial Intelligence:\t{taken_max}\n')
-    text1.insert(END, f'Remainder after Artificial Intelligence:\t{amount}\n')     
+    text1.insert(END, f'Remainder after Artificial Intelligence:\t{amount}\n')
     if amount == 0 or amount < 0:
         text1.insert(END, 'Amigo wins;)')
-        amount = random.randint(19, 30)
+        amount = random.randint(13,30)
     if amount == 1:
         text1.insert(END, 'Sorry Amigo, but remainder 1. You lose!\n\t\t\tRestart?')
-        amount = random.randint(19, 30)
-
+        amount = random.randint(13,30)
+ 
 
 def start():
     if btn_start['text'] == 'Start':
@@ -88,13 +91,13 @@ pb.stop()
 
 
 def restart():
-    text1.delete('1.0', END)
-    e.delete(0, END)
     if btn_res['text'] == 'Restart':
+        text1.delete('1.0', END)
+        e.delete(0, END)
         text1.insert(END, f'\nHow many sticks do you want to take Amigo?\n\tRandom number to start: {amount}!\n')
+    
 
-
-frame2=Frame(root, bg='#1F252A', bd=5)
+frame2=Frame(root, bg='#1F252A')
 frame2.pack(fill=X)
 
 
@@ -104,23 +107,19 @@ s = t.Style()
 s.configure('Kim.TButton', foreground='maroon')
 
 for k, v in buttons.items():
-    t.Button(frame2, text=k, width=20, style='Kim.TButton', command=lambda number=v: get_num(number)).pack(fill=BOTH, side=LEFT)
+    t.Button(frame2, text=k, style='Kim.TButton', command=lambda number=v: get_num(number)).pack(fill=X, expand=1, side=LEFT)
 
-frame1=Frame(root, bg='#1F252A', bd=5)
+frame1=Frame(root, bg='#1F252A')
 frame1.pack(fill=X)
 
 btn_start = t.Button(frame1, text='Start', style='Kim.TButton', command=start)
-btn_start.grid(row=0, column=1, ipadx=27)
+btn_start.pack(fill=X, expand=1, side=LEFT)
 
-btn_res = t.Button(frame1, text='Restart', style='Kim.TButton' , command=restart)
-btn_res.grid(row=0, column=2, ipadx=27)
+btn_res = t.Button(frame1, text='Restart', style='Kim.TButton', command=restart)
+btn_res.pack(fill=X, expand=1, side=LEFT)
 
 btn_exit = t.Button(frame1, text='Exit', style='Kim.TButton', command=quit)
-btn_exit.grid(row=0, column=3, ipadx=27)
-
-
-label = Label(root, text='Tabulistan is proud to be an Official Sponsor.', font='Arial, 9')
-label.pack()
+btn_exit.pack(fill=X, expand=1, side=LEFT)
 
 
 if __name__ == "__main__":
